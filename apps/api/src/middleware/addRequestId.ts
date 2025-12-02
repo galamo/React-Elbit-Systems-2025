988d2752af1421085c26b72dfde622e6937d76d0
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2025-12-02 13:33:21 +0200

    a

--- apps/api/src/middleware/addRequestId.ts
+++ apps/api/src/middleware/addRequestId.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as gioud } from 'uuid';
 import { ReqLocal } from "./authorizationMiddleware";
 
 
--- readme.installations.md
+++ readme.installations.md
@@ -6,3 +6,7 @@
 ## Slides
 
 https://gamma.app/docs/TypeScript-e3v9su0jf3qrion?mode=present#card-iwrhvd826qm8ls3
+
+
+
+workers 
